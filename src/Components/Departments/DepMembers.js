41d9c8b858c6fb5ef0ff2ac6a112de5fa53345ca
Mostafa SM <mostafa.sotoodeh@gmail.com>
2022-01-18 00:17:07 +0330

    minor bugfix

--- src/Components/Departments/DepMembers.js
+++ src/Components/Departments/DepMembers.js
@@ -11,10 +11,10 @@ export default function DepMembers(props) {
             <Col lg={4} xs={12} >
                 <div className='memberCard'  onMouseOver={() => setShowBio(true) } onMouseLeave={() => setShowBio(false)}>
                     <img src={image}/>
-                    <div className={`overlayData ${showBio ? 'open' : 'close'}`} >
+                    <div className={`overlayData`} >
                         <span className='memberName'> {name} </span> <br/>
                         <span className='memberPosition'> {position} </span>
-                        <p className={`memberBio ${showBio ? 'open' : 'close'}`}> {bio} </p>
+                        {/* <p className={`memberBio ${showBio ? 'open' : 'close'}`}> {bio} </p> */}
                     </div>
                 </div>
             </Col>
