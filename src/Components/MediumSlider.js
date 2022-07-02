40aa520b62fd2f03846dcc0112ec98f1438421ca
unknown <rezabahri2001>
2022-07-02 06:08:32 -0700

    Change Link

--- src/Components/MediumSlider.js
+++ src/Components/MediumSlider.js
@@ -12,12 +12,22 @@ class Slider extends React.Component {
   mediumURL =
     "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@LivelyVerse";
 
+
   async componentDidMount() {
+
     await axios
       .get(this.mediumURL)
       .then(async (res) => await res.data)
       .then((data) => {
+        console.log("medium data", data);
         // create two-dimensional array with 3 elements per inner array
+        {
+          data.items.map((count) => {
+            if (count.link == "https://medium.com/@LivelyVerse/livelyworld-the-new-program-dedicated-to-blockchain-technology-6751f0692f3e?source=rss-a117dad72655------2") {
+              count.link = "https://medium.com/@LivelyVerse/lively-world-the-new-tv-show-dedicated-to-blockchain-technology-be2597df7945?source=rss-a117dad72655------2"
+            }
+          })
+        }
         const avatar = data.feed.image;
         const profileLink = data.feed.link;
         const res = data.items; //This is an array with the content. No feed, no info about author etc..
